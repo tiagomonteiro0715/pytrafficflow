d985259adcab532ad859fb9b8678eb51aa74e8e0
Tiago monteiro <tiago.monteiro0715@gmail.com>
2025-10-09 21:52:03 -0700

    Delete core directory

--- core/base_model.py
+++
@@ -1,6 +0,0 @@
-"""
-Base model class for traffic flow simulations.
-This module will contain the abstract base class that all traffic models should inherit from.
-"""
-
-# TODO: Implement BaseTrafficModel abstract class
