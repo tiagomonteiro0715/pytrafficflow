0194b9a83083da7541b6dfe1841f9d2a32eee3db
Tiago monteiro <tiago.monteiro0715@gmail.com>
2025-10-09 21:51:56 -0700

    Delete data directory

--- data/loader.py
+++
@@ -1,6 +0,0 @@
-"""
-Data loading utilities for traffic flow simulations.
-This module will contain functions to load traffic data from various sources.
-"""
-
-# TODO: Implement data loading functions for different data formats
--- data/preprocess.py
+++
@@ -1,6 +0,0 @@
-"""
-Data preprocessing utilities for traffic flow simulations.
-This module will contain functions to clean and prepare traffic data for analysis.
-"""
-
-# TODO: Implement data preprocessing functions (cleaning, normalization, etc.)
